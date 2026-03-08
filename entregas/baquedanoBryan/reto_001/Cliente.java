6ca48942013996f43c1fa2d1dc1761e5dd8f25f3
Bryan Baquedano <144849127+enmabry@users.noreply.github.com>
2026-03-08 22:13:43 +0100

    eliminar método disminuirItems no utilizado
    disminuirItems() estaba implementado en Cliente pero no era invocado en ningún punto del proyecto. Ninguna clase lo necesitaba ni fue pedido por el enunciado. Eliminado para cumplir el principio YAGNI.

--- entregas/baquedanoBryan/reto_001/Cliente.java
+++ entregas/baquedanoBryan/reto_001/Cliente.java
@@ -18,12 +18,6 @@ public class Cliente{
         }
     }
 
-    public void disminuirItems(int cantidad) {
-        if (cantidad > 0 && this.numItems >= cantidad) {
-            this.numItems -= cantidad;
-        }
-    }
-
     @Override
     public String toString() {
         return "Cliente con " + numItems + " ítems.";
